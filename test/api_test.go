d757ee9988ea9f9a72a96d7ba761cf0d887917eb
lihongwei <lihongwei@lihongweideMacBook-Pro.local>
2023-02-28 23:48:48 +0800

    s

--- test/api_test.go
+++ test/api_test.go
@@ -1,6 +1,7 @@
-package api
+package test
 
 import (
+	"my-server-go/api"
 	"testing"
 )
 
@@ -9,6 +10,6 @@ func TestOne(t *testing.T) {
 	var timestamp = "1677596202"
 	var nonce = "1676975221"
 	var data = "<xml><ToUserName><![CDATA[ww8d5186f5aa839ee7]]></ToUserName><Encrypt><![CDATA[MMs94DR1S7fGleh0mKyjA3RNgwuNNGu0YyGimD+99GB16gCpuhBkvWxaTt20L1PC6Ni0VBWnpSdlpUsWseUbFpmsRtt8aFkTdoyRBe8C0gx9hM8bLrOWGcdOJrtXaGIUnOF8H8UuinQXLjO/uAulBKLKE7TiMFXtvaQ62/Iuzc5UKdh8bAbGUk+iOY1nUkh3L5BSPpyWHWVKEFyLkumjUCWZV4L11lSuG9nqbDVVFhdHLT/Du3TCX/To4DW7DIUyjgpzARVjAPzBzGvYYe1Nq1Y3RkjbwdWRWz824xhgmYEpiUr4XOYlfqTWljydXOV+NdNmJBXc/WDnG4u2jo1HsUjYRsWzaYqux4CX3dm1WI6L9iDJB87F5Ldp90yoxuf5rBdb3xLtssxBu8S4zievwlZVzRnQWN33Xvg0fUKHjo0=]]></Encrypt><AgentID><![CDATA[1000002]]></AgentID></xml>"
-	ProcessMessage(msg_signature, timestamp, nonce, []byte(data))
+	api.ProcessMessage(msg_signature, timestamp, nonce, []byte(data))
 
 }

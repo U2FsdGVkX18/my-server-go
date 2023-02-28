0cce54145e2494aa43acd5daa056e05bfec985f5
lihongwei <lihongwei@lihongweideMacBook-Pro.local>
2023-02-28 23:47:26 +0800

    add

---
+++ api/api_test.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestOne(t *testing.T) {
+	var msg_signature = "bb65bc2127f86d862df8e6917daa7ef4a7b1733d"
+	var timestamp = "1677596202"
+	var nonce = "1676975221"
+	var data = "<xml><ToUserName><![CDATA[ww8d5186f5aa839ee7]]></ToUserName><Encrypt><![CDATA[MMs94DR1S7fGleh0mKyjA3RNgwuNNGu0YyGimD+99GB16gCpuhBkvWxaTt20L1PC6Ni0VBWnpSdlpUsWseUbFpmsRtt8aFkTdoyRBe8C0gx9hM8bLrOWGcdOJrtXaGIUnOF8H8UuinQXLjO/uAulBKLKE7TiMFXtvaQ62/Iuzc5UKdh8bAbGUk+iOY1nUkh3L5BSPpyWHWVKEFyLkumjUCWZV4L11lSuG9nqbDVVFhdHLT/Du3TCX/To4DW7DIUyjgpzARVjAPzBzGvYYe1Nq1Y3RkjbwdWRWz824xhgmYEpiUr4XOYlfqTWljydXOV+NdNmJBXc/WDnG4u2jo1HsUjYRsWzaYqux4CX3dm1WI6L9iDJB87F5Ldp90yoxuf5rBdb3xLtssxBu8S4zievwlZVzRnQWN33Xvg0fUKHjo0=]]></Encrypt><AgentID><![CDATA[1000002]]></AgentID></xml>"
+	ProcessMessage(msg_signature, timestamp, nonce, []byte(data))
+
+}
---
+++ api/api_wechat_access.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"github.com/gin-gonic/gin"
+	"log"
+	logger "my-server-go/commons/tools/log"
+	"my-server-go/commons/tools/wechataes/wxmsgcrypt"
+	"net/http"
+)
+
+const sCorpID = "ww8d5186f5aa839ee7"
+
+const sToken = "RlWIx3oywb6O"
+
+const sEncodingAESKey = "YE7McxpnpLYFAO8qIxrkjbxqjTjt6rfSjBeEiV3YDNv"
+
+type WeChatReqMsgInfo struct {
+	XMLName    xml.Name `xml:"xml"`
+	ToUserName string   `xml:"ToUserName"`
+	AgentID    string   `xml:"AgentID"`
+	Encrypt    string   `xml:"Encrypt"`
+}
+
+type MsgContent struct {
+	ToUsername   string `json:"ToUserName"`
+	FromUsername string `json:"FromUserName"`
+	CreateTime   uint32 `json:"CreateTime"`
+	MsgType      string `json:"MsgType"`
+	Content      string `json:"Content"`
+	Msgid        uint64 `json:"MsgId"`
+	Agentid      uint32 `json:"AgentId"`
+}
+
+func WeChatAccess(ginServer *gin.Engine) {
+	var wechatGroup = ginServer.Group("/wechat")
+	{
+		wechatGroup.GET("/recall", func(context *gin.Context) {
+			var msg_signature = context.Query("msg_signature")
+			var timestamp = context.Query("timestamp")
+			var nonce = context.Query("nonce")
+			var echostr = context.Query("echostr")
+			logger.Write("接口参数为:", msg_signature, timestamp, nonce, echostr)
+			//验证URL
+			echoStr := VerifyUrl(msg_signature, timestamp, nonce, echostr)
+			context.String(http.StatusOK, echoStr)
+		})
+		wechatGroup.POST("/recall", func(context *gin.Context) {
+			var msg_signature = context.Query("msg_signature")
+			var timestamp = context.Query("timestamp")
+			var nonce = context.Query("nonce")
+			post_data, _ := context.GetRawData()
+			logger.Write("接口参数为:", msg_signature, timestamp, nonce, string(post_data))
+			//处理消息
+			ProcessMessage(msg_signature, timestamp, nonce, post_data)
+
+		})
+	}
+}
+
+func VerifyUrl(msg_signature string, timestamp string, nonce string, echostr string) string {
+	wxcpt := wxmsgcrypt.NewWXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID, wxmsgcrypt.JsonType)
+	echoStr, cryptError := wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
+	if cryptError != nil {
+		logger.Write("VerifyUrl 验证失败!", cryptError)
+	}
+	logger.Write("VerifyUrl 验证成功!", "resp:echoStr:"+string(echoStr))
+	return string(echoStr)
+}
+
+func ProcessMessage(msg_signature string, timestamp string, nonce string, post_data []byte) {
+	wxcpt := wxmsgcrypt.NewWXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID, wxmsgcrypt.JsonType)
+	msg, cryptError := wxcpt.DecryptMsg(msg_signature, timestamp, nonce, post_data)
+	if cryptError != nil {
+		log.Println("ProcessMessage 消息处理失败!", cryptError)
+		logger.Write("ProcessMessage 消息处理失败!", cryptError)
+	}
+
+	//创建新的对象
+	var msgContent MsgContent
+
+	err := json.Unmarshal(msg, &msgContent)
+	if err != nil {
+		log.Println("ProcessMessage 消息解析失败!", err)
+		logger.Write("ProcessMessage 消息解析失败!", err)
+	} else {
+		log.Println("ProcessMessage 消息解析成功!", msgContent)
+		logger.Write("ProcessMessage 消息解析成功!", msgContent)
+	}
+
+}

2e78435acabcf3f065729c5fe069ed159af2c2d8
lihongwei <lihongwei@lihongweideMBP.lan>
2023-03-02 12:19:43 +0800

    add

--- api/api_wechat_access.go
+++ api/api_wechat_access.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"github.com/gin-gonic/gin"
-	"log"
 	logger "my-server-go/tool/log"
 	"my-server-go/tool/wechataes/wxmsgcrypt"
 	"net/http"
@@ -16,12 +15,14 @@ const sToken = "RlWIx3oywb6O"
 
 const sEncodingAESKey = "YE7McxpnpLYFAO8qIxrkjbxqjTjt6rfSjBeEiV3YDNv"
 
+// WeChatReqMsgInfo 接收到的消息加密格式
 type WeChatReqMsgInfo struct {
 	ToUserName string
 	AgentID    string
 	Encrypt    string
 }
 
+// MsgContent 接收到的消息经过解密后的格式
 type MsgContent struct {
 	ToUsername   string  `xml:"ToUserName"`
 	FromUsername string  `xml:"FromUserName"`
@@ -60,12 +61,13 @@ func WeChatAccess(ginServer *gin.Engine) {
 			post_data, _ := context.GetRawData()
 			logger.Write("接口参数为:", msg_signature, timestamp, nonce, string(post_data))
 			//处理消息
-			ProcessMessage(msg_signature, timestamp, nonce, post_data)
-
+			message := ProcessMessage(msg_signature, timestamp, nonce, post_data)
+			context.String(http.StatusOK, message)
 		})
 	}
 }
 
+// VerifyUrl 验证url
 func VerifyUrl(msg_signature string, timestamp string, nonce string, echostr string) string {
 	wxcpt := wxmsgcrypt.NewWXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID, wxmsgcrypt.JsonType)
 	echoStr, cryptError := wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr)
@@ -76,14 +78,15 @@ func VerifyUrl(msg_signature string, timestamp string, nonce string, echostr str
 	return string(echoStr)
 }
 
-func ProcessMessage(msg_signature string, timestamp string, nonce string, post_data []byte) {
+// ProcessMessage 处理接收到的消息
+func ProcessMessage(msg_signature string, timestamp string, nonce string, post_data []byte) string {
 	wxcpt := wxmsgcrypt.NewWXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID, wxmsgcrypt.JsonType)
 	//定义结构体
 	var weChatReqMsgInfo WeChatReqMsgInfo
 	//将返回的xml转为结构体
 	err := xml.Unmarshal(post_data, &weChatReqMsgInfo)
 	if err != nil {
-		return
+		logger.Write("ProcessMessage xml解析失败!", err)
 	}
 	//将结构体转为json
 	data, err := json.Marshal(weChatReqMsgInfo)
@@ -92,7 +95,6 @@ func ProcessMessage(msg_signature string, timestamp string, nonce string, post_d
 	if cryptError != nil {
 		logger.Write("ProcessMessage 消息处理失败!", *cryptError)
 	} else {
-		log.Println("ProcessMessage 消息处理成功!", string(msg))
 		logger.Write("ProcessMessage 消息处理成功!", string(msg))
 	}
 
@@ -103,14 +105,26 @@ func ProcessMessage(msg_signature string, timestamp string, nonce string, post_d
 	if err != nil {
 		logger.Write("ProcessMessage 消息解析失败!", err)
 	} else {
-		log.Println("ProcessMessage 消息解析成功!", msgContent)
 		logger.Write("ProcessMessage 消息解析成功!", msgContent)
 	}
 
 	switch msgContent.MsgType {
 	case "text":
-		log.Println("ProcessMessage 检测到机器人接收到消息的内容:", msgContent.Content)
 		logger.Write("ProcessMessage 检测到机器人接收到消息的内容:", msgContent.Content)
+		username := weChatReqMsgInfo.ToUserName
+		switch msgContent.Content {
+		case "能力":
+			str := "【赋予能力✅】" + "\n" +
+				"\n" +
+				"暂无" +
+				"\n" +
+				""
+			text := CreatePassiveRespText(username, timestamp, nonce, str)
+			return text
+		default:
+			text := CreatePassiveRespText(username, timestamp, nonce, "未知")
+			return text
+		}
 	case "event":
 		if msgContent.Event == "LOCATION" {
 			var Latitude = msgContent.Latitude
@@ -118,4 +132,55 @@ func ProcessMessage(msg_signature string, timestamp string, nonce string, post_d
 			logger.Write("ProcessMessage 检测到机器人接收到的位置信息:", Latitude, Longitude)
 		}
 	}
+	return ""
+}
+
+type WeChatPassiveRespMsgInfo struct {
+	XMLName      xml.Name `xml:"xml"`
+	Encrypt      string   `xml:"encrypt" json:"Encrypt"`
+	MsgSignature string   `xml:"msgSignature" json:"MsgSignature"`
+	TimeStamp    string   `xml:"timeStamp" json:"TimeStamp"`
+	Nonce        string   `xml:"nonce" json:"Nonce"`
+}
+
+// CreatePassiveRespText 构造被动消息
+func CreatePassiveRespText(username string, timestamp string, nonce string, content string) string {
+	//拿到封装好的被动消息结构体
+	weChatPassiveRespMsg := GetPassiveRespMessageBodyText(username, timestamp, "text", content)
+	//将被动消息结构体转为xml
+	weChatPassiveRespMsgXml, _ := xml.Marshal(weChatPassiveRespMsg)
+	logger.Write("weChatPassiveRespMsg :", string(weChatPassiveRespMsgXml))
+	//将该xml进行加密,得到一个json
+	wxcpt := wxmsgcrypt.NewWXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID, wxmsgcrypt.JsonType)
+	msg, _ := wxcpt.EncryptMsg(string(weChatPassiveRespMsgXml), timestamp, nonce)
+	logger.Write("weChatPassiveRespMsg EncryptMsg:", string(msg))
+	var weChatPassiveRespMsgInfo WeChatPassiveRespMsgInfo
+	//将json转为结构体
+	_ = json.Unmarshal(msg, &weChatPassiveRespMsgInfo)
+	//将结构体转为xml返回
+	marshal, _ := xml.Marshal(weChatPassiveRespMsgInfo)
+	logger.Write("weChatPassiveRespMsg weChatPassiveRespMsgInfo:", string(marshal))
+	return string(marshal)
+}
+
+// WeChatPassiveRespMsg 被动响应消息结构体
+type WeChatPassiveRespMsg struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUsername   string   `xml:"ToUserName"`
+	FromUsername string   `xml:"FromUserName"`
+	CreateTime   string   `xml:"CreateTime"`
+	MsgType      string   `xml:"MsgType"`
+	Content      string   `xml:"Content"`
+}
+
+// GetPassiveRespMessageBodyText 封装被动响应消息格式
+func GetPassiveRespMessageBodyText(userName string, createTime string, messageType string, content string) WeChatPassiveRespMsg {
+	weChatPassiveRespMsg := WeChatPassiveRespMsg{
+		ToUsername:   userName,
+		FromUsername: userName,
+		CreateTime:   createTime,
+		MsgType:      messageType,
+		Content:      content,
+	}
+	return weChatPassiveRespMsg
 }
--- test/api_test.go
+++ test/api_test.go
@@ -12,3 +12,7 @@ func TestOne(t *testing.T) {
 	var data = "<xml><ToUserName><![CDATA[ww8d5186f5aa839ee7]]></ToUserName><Encrypt><![CDATA[MMs94DR1S7fGleh0mKyjA3RNgwuNNGu0YyGimD+99GB16gCpuhBkvWxaTt20L1PC6Ni0VBWnpSdlpUsWseUbFpmsRtt8aFkTdoyRBe8C0gx9hM8bLrOWGcdOJrtXaGIUnOF8H8UuinQXLjO/uAulBKLKE7TiMFXtvaQ62/Iuzc5UKdh8bAbGUk+iOY1nUkh3L5BSPpyWHWVKEFyLkumjUCWZV4L11lSuG9nqbDVVFhdHLT/Du3TCX/To4DW7DIUyjgpzARVjAPzBzGvYYe1Nq1Y3RkjbwdWRWz824xhgmYEpiUr4XOYlfqTWljydXOV+NdNmJBXc/WDnG4u2jo1HsUjYRsWzaYqux4CX3dm1WI6L9iDJB87F5Ldp90yoxuf5rBdb3xLtssxBu8S4zievwlZVzRnQWN33Xvg0fUKHjo0=]]></Encrypt><AgentID><![CDATA[1000002]]></AgentID></xml>"
 	api.ProcessMessage(msg_signature, timestamp, nonce, []byte(data))
 }
+
+func TestTwo(t *testing.T) {
+	api.CreatePassiveRespText("lihongwei", "123123123", "nonce", "1")
+}
